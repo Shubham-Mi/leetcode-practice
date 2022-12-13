3c8ecb581642accb95e537dbbc626265fb7e9909
Shubham Mittal <shubham.mitt112@gmail.com>
2022-12-13 14:57:14 +0530

    Time: 155 ms (90.95%), Space: 65.4 MB (69.00%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,49 +1,9 @@
 class Solution {
 public:
-    bool check(vector <int>& stones, int n, int mid) {
-        vector <bool> visited(n, false);
-        int curr = 0;
-        int prv = 0;
-        while (curr < n) {
-            if (prv == n-1) break;
-            while (curr + 1 < n && stones[curr + 1] - stones[prv] <= mid) {
-                curr++;
-            }
-            while (curr != prv && visited[curr]) {
-                curr--;
-            }
-            if (curr == prv) return false;
-            visited[curr] = true;
-            prv = curr;
-        }
-        while (curr >= 0) {
-            if (prv == 0) break;
-            while (curr - 1 >= 0 && stones[prv] - stones[curr - 1] <= mid) {
-                curr--;
-            }
-            while (curr != prv && visited[curr]) {
-                curr++;
-            }
-            if (curr == prv) return false;
-            visited[curr] = true;
-            prv = curr;
-        }
-        return true;
-    }
     int maxJump(vector<int>& stones) {
         int n = stones.size();
-        int low = 1;
-        int high = stones[n - 1];
-        int ans = high;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (check(stones, n, mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
+        int ans = stones[1];
+        for (int i=2; i<n; ++i) ans = max(ans, stones[i] - stones[i - 2]);
         return ans;
     }
 };
