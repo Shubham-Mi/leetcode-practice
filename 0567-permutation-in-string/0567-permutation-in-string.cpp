ba259b67c12cb716e2274c4ec39a7237a99cfd44
Shubham Mittal <shubham.mitt112@gmail.com>
2023-02-05 15:46:35 +0530

    Time: 4 ms (93.32%), Space: 7.1 MB (98.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,31 +3,18 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.size();
         int m = s2.size();
-        if (n > m) return false;
-        vector <int> cnt(26, 0);
-        for (int i=0; i<n; ++i) {
-            cnt[s1[i] - 'a']--;
-            cnt[s2[i] - 'a']++;
-        }
-        bool found = false;
-        for (int i=0; i<26; ++i) {
-            if (cnt[i] != 0) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) return true;
-        for (int i=n; i<m; ++i) {
-            cnt[s2[i] - 'a']++;
-            cnt[s2[i - n] - 'a']--;
-            found = false;
-            for (int i=0; i<26; ++i) {
-                if (cnt[i] != 0) {
+        vector <int> count(26, 0);
+        for (char c: s1) count[c - 'a']--;
+        for (int i=0; i<m; ++i) {
+            count[s2[i] - 'a']++;
+            if (i >= n) count[s2[i - n] - 'a']--;
+            bool found = false;
+            for (int num: count) {
+                if (num != 0) {
                     found = true;
                     break;
                 }
             }
-            
             if (!found) return true;
         }
         return false;
