34fbb2be2eb8bfa37203b39c10cde762a5b7c80d
Shubham Mittal <shubham.mitt112@gmail.com>
2023-04-10 12:16:12 +0530

    Time: 4 ms (39.51%), Space: 6.3 MB (56.25%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> stk;
+        for (int i=0; i<n; ++i) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') stk.push(s[i]);
+            else if (s[i] == ')') {
+                if (stk.empty() || stk.top() != '(')
+                    return false;
+                stk.pop();
+            } else if (s[i] == '}') {
+                if (stk.empty() || stk.top() != '{')
+                    return false;
+                stk.pop();
+            } else if (s[i] == ']') {
+                if (stk.empty() || stk.top() != '[')
+                    return false;
+                stk.pop();
+            }
+        }
+        if (stk.empty())
+            return true;
+        return false;
+    }
+};
